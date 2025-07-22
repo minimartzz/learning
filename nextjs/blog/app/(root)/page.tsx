051ed5fe3e814c0a0129082c68a40543410bbfd6
Minimartzz <martin.ho.zy97@gmail.com>
2025-07-23 00:17:12 +0800

    Update: Stopped at 2:12:30

--- nextjs/blog/app/(root)/page.tsx
+++ nextjs/blog/app/(root)/page.tsx
@@ -1,9 +1,56 @@
 import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Martin" },
+      _id: 1,
+      description: "This is a description",
+      image: "https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
     <>
-      <h1 className="heading">HOME</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup,
+          <br />
+          Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
